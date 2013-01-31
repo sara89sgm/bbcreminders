05f41cf08bc0d892249045d1805cf50971a23008
Sara Gozalo <sara89sgm@gmail.com>
2013-01-31 15:22:28 +0000

    Parse problems shitty fb

--- index.php
+++ index.php
@@ -175,8 +175,7 @@ $app_name = idx($app_info, 'name', '');
     function _showSchedule(schedule){
       var programmes=schedule.schedule.day.broadcasts;
       console.log("Programme",programmes);
-      var list = '#'+schedule.schedule.service.key;
-      $(list).append('holaaa'+programmes[0].programme.programme.title);
+      $("#r1Schedule").append('holaaa'+programmes[0].programme.programme.title);
 
     }
 
