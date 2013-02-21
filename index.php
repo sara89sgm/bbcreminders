e9ccae6b1e477c50cc92c19a2f24f722e393d184
Sara Gozalo <sara89sgm@gmail.com>
2013-02-21 12:41:27 +0000

    removing require.js

--- index.php
+++ index.php
@@ -11,8 +11,6 @@
 // Provides access to app specific values such as your app id and app secret.
 // Defined in 'AppInfo.php'
 require_once('AppInfo.php');
-echo("rerere");
-exit();
 
 // Enforce https on production
 if (substr(AppInfo::getUrl(), 0, 8) != 'https://' && $_SERVER['REMOTE_ADDR'] != '127.0.0.1') {
@@ -107,10 +105,10 @@ $app_name = idx($app_info, 'name', '');
     <meta property="og:site_name" content="<?php echo he($app_name); ?>" />
     <meta property="og:description" content="My first app" />
     <meta property="fb:app_id" content="<?php echo AppInfo::appID(); ?>" />
-   <script type="text/javascript" src="/javascript/jquery-1.7.1.min.js"></script>
- 
-    <script type="text/javascript" src="/javascript/parse-1.2.0.js"></script>
   
+             <script type="text/javascript" src="/javascript/jquery-1.7.1.min.js"></script>
+ 
+ 
  
   </head>
   <body>
@@ -118,7 +116,7 @@ $app_name = idx($app_info, 'name', '');
 
      <script>
         Parse.initialize("5LEqnxjXggAyQZWae2B4l6fCbUFTWny1NfX0Ka7Q", "HlpTncUyPpzJUfYpLgTzkHPQcthMYK8Y9IcHjNvi");
-      console.log("hello");
+      
        
             (function(d, s, id) {
               var js, fjs = d.getElementsByTagName(s)[0];
@@ -128,7 +126,7 @@ $app_name = idx($app_info, 'name', '');
               fjs.parentNode.insertBefore(js, fjs);
               console.log("fbbbb");
           }(document, 'script', 'facebook-jssdk'));
-         // Controller_init();
+          Controller_init();
         
 
     </script>
@@ -199,7 +197,101 @@ $app_name = idx($app_info, 'name', '');
       
     </section>
 
-  
+    <section id="samples" class="clearfix">
+      <h1>More info</h1>
+
+      <div class="list">
+        <h3>A few of your friends</h3>
+        <ul class="friends">
+          <?php
+            foreach ($friends as $friend) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($friend, 'id');
+              $name = idx($friend, 'name');
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+              <?php echo he($name); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list inline">
+        <h3>Recent photos</h3>
+        <ul class="photos">
+          <?php
+            $i = 0;
+            foreach ($photos as $photo) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($photo, 'id');
+              $picture = idx($photo, 'picture');
+              $link = idx($photo, 'link');
+
+              $class = ($i++ % 4 === 0) ? 'first-column' : '';
+          ?>
+          <li style="background-image: url(<?php echo he($picture); ?>);" class="<?php echo $class; ?>">
+            <a href="<?php echo he($link); ?>" target="_top"></a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Things you like</h3>
+        <ul class="things">
+          <?php
+            foreach ($likes as $like) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($like, 'id');
+              $item = idx($like, 'name');
+
+              // This display's the object that the user liked as a link to
+              // that object's page.
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($item); ?>">
+              <?php echo he($item); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+
+      <div class="list">
+        <h3>Friends using this app</h3>
+        <ul class="friends">
+          <?php
+            foreach ($app_using_friends as $auf) {
+              // Extract the pieces of info we need from the requests above
+              $id = idx($auf, 'uid');
+              $name = idx($auf, 'name');
+          ?>
+          <li>
+            <a href="https://www.facebook.com/<?php echo he($id); ?>" target="_top">
+              <img src="https://graph.facebook.com/<?php echo he($id) ?>/picture?type=square" alt="<?php echo he($name); ?>">
+              <?php echo he($name); ?>
+            </a>
+          </li>
+          <?php
+            }
+          ?>
+        </ul>
+      </div>
+    </section>
+
+    <?php
+      }
+    ?>
 
     
     
