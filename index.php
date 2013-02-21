d95b45f5191b4c86d9489da25330bbf5d76027e7
Sara Gozalo <sara89sgm@gmail.com>
2013-02-21 12:55:02 +0000

    removing require.js

--- index.php
+++ index.php
@@ -121,8 +121,8 @@ $app_name = idx($app_info, 'name', '');
         Parse.initialize("5LEqnxjXggAyQZWae2B4l6fCbUFTWny1NfX0Ka7Q", "HlpTncUyPpzJUfYpLgTzkHPQcthMYK8Y9IcHjNvi");
         window.fbAsyncInit = function() {
           FB.init({
-            appId      : 'YOUR_APP_ID', // App ID
-            channelUrl : '//WWW.YOUR_DOMAIN.COM/channel.html', // Channel File
+            appId      : '<?php echo AppInfo::appID(); ?>', // App ID
+            channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
             status     : true, // check login status
             cookie     : true, // enable cookies to allow the server to access the session
             xfbml      : true  // parse XFBML
@@ -135,6 +135,16 @@ $app_name = idx($app_info, 'name', '');
               name.innerHTML = user.name
             }
           });
+            FB.Event.subscribe('auth.login', function(response) {
+          // We want to reload the page now so PHP can read the cookie that the
+          // Javascript SDK sat. But we don't want to use
+          // window.location.reload() because if this is in a canvas there was a
+          // post made to this page and a reload will trigger a message to the
+          // user asking if they want to send data again.
+          window.location = window.location;
+        });
+
+        FB.Canvas.setAutoGrow();
         };
         // Load the SDK Asynchronously
         (function(d){
